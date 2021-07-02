2a1b5868ee32c5ccb79f75f9c16fdc95c1e41625
budds300 <budds300@gmail.com>
2021-07-02 22:16:08 +0300

    edited settings

--- Album/settings.py
+++ Album/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e-r&+&3_a&tbj6pzk+a3+)1*^u68=tsu+#^^49i2-jz6d-b!0='
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
