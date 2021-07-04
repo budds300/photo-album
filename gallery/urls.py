b96f0002978f6cabff5dc8386523a7abd1ca2edd
budds300 <budds300@gmail.com>
2021-07-04 21:52:14 +0300

    updated url file

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,18 +1,22 @@
-from django.conf.urls.static import static
-from django.conf.urls import url
-from django.urls import path
-from . import views
-from django.conf import settings
+"""gallery URL Configuration
 
-# from django.conf.urls.static import static
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
 
 urlpatterns = [
-    path('',views.gallery, name='gallery'),
-    # path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_day_picture,name = 'pastDayPicture'),
-    # path('', views.search_by_category,name='search_results'),
-    # path('pic/<int:image_id>', views.photos, name = 'photo'),
-    url(r'^photo/(\d+)',views.photos,name='photo'),
-    url(r'^search/$', views.search_results, name='search_results')
+    url(r'^admin/', admin.site.urls),
+    url(r'',include('pics.urls'))
 ]
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
