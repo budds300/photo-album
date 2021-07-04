fde7567e5e9c4f8c25420c57aa1b6edc52adf9b8
budds300 <budds300@gmail.com>
2021-07-04 14:12:40 +0300

    addition of search ,debug gallery and individual photo and search

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,18 @@
+from django.conf.urls.static import static
 from django.conf.urls import url
+from django.urls import path
 from . import views
+from django.conf import settings
+
 # from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$',views.gallery, name='gallery')
+    path('',views.gallery, name='gallery'),
+    # path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_day_picture,name = 'pastDayPicture'),
+    # path('', views.search_by_category,name='search_results'),
+    # path('pic/<int:image_id>', views.photos, name = 'photo'),
+    url(r'^photo/(\d+)',views.photos,name='photo'),
+    url(r'^search/$', views.search_results, name='search_results')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
