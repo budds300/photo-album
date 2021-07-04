3bddfae22bf75156c93e7ebb8ba6bc705801291f
budds300 <budds300@gmail.com>
2021-07-04 14:11:38 +0300

    updating of admin in the app

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Photo,Location,Cartegory
 # Register your models here.
+
+
+    
+admin.site.register(Photo)
+admin.site.register(Location)
+admin.site.register(Cartegory)
