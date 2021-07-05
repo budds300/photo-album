5493b77dbaa77bd5164c1fe6421bf01a0f79dcd3
budds300 <budds300@gmail.com>
2021-07-05 11:02:15 +0300

    deployment of heroku

--- gallery/settings.py
+++ gallery/settings.py
@@ -105,7 +105,14 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'photos',
+        'USER': 'moringa',
+        'PASSWORD':'hello',
+    }
+}
 
 
 
