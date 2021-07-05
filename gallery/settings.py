da6c5c204196ca2fedab3d43a74ee7ff44cf9ada
budds300 <budds300@gmail.com>
2021-07-05 08:43:25 +0300

    Heroku deployment

--- gallery/settings.py
+++ gallery/settings.py
@@ -156,9 +156,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
 
-cloudinary.config( 
-  cloud_name = config('CLOUD_NAME'), 
-  api_key = config('API_KEY'),
-  api_secret = config('API_SECRET'),
+# cloudinary.config( 
+#   cloud_name = config('CLOUD_NAME'), 
+#   api_key = config('API_KEY'),
+#   api_secret = config('API_SECRET'),
   
-)
+# )
